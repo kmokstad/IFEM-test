0d9b7840cb36b2bb4cc9a014341d7d6e348b39b9
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2011-04-28 14:56:20 +0000

    Single-patch input files
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@919 e10b68d5-8a6e-419e-a041-bce267b0401d

--- Necking/run.sh
+++ Necking/run.sh
@@ -4,20 +4,25 @@ if [ $# -gt 0 ]; then h=$1; else exec echo "usage: $0 <h>"; fi
 
 runNonlinel necking-2D-$h-Q1.inp   -2D -UL -vtf 1 -nGauss 2 -outPrec 6
 runNonlinel necking-2D-$h-Q1P0.inp -2D -MX 0 -vtf 1 -nGauss 2 -outPrec 6
+runNonlinel Necking-2D-$h-Q1P0.inp -2D -MX 0 -vtf 1 -nGauss 2 -outPrec 6
 
 runNonlinel necking-2D-$h-Q2.inp   -2D -UL -vtf 1 -lagrange -nGauss 3 -outPrec 6
 runNonlinel necking-2D-Q2-$h.inp   -2D -UL -vtf 1 -nviz 3   -nGauss 3 -outPrec 6
 runNonlinel necking-2D-$h-Q2P1.inp -2D -MX 1 -vtf 1 -lagrange -nGauss 3 -outPrec 6 
 runNonlinel necking-2D-Q2P1-$h.inp -2D -MX 1 -vtf 1 -nviz 3   -nGauss 3 -outPrec 6
+runNonlinel Necking-2D-Q2P1-$h.inp -2D -MX 1 -vtf 1 -nviz 3   -nGauss 3 -outPrec 6
 runNonlinel necking-2D-$h-Q2Q1.inp -2D -mixed -vtf 1 -lagrange -nGauss 3 -outPrec 6
 runNonlinel necking-2D-Q2Q1-$h.inp -2D -mixed -vtf 1 -nviz 3   -nGauss 3 -outPrec 6
+runNonlinel Necking-2D-Q2Q1-$h.inp -2D -mixed -vtf 1 -nviz 3   -nGauss 3 -outPrec 6
 
 runNonlinel necking-2D-$h-Q3.inp -2D -UL -vtf 1 -lagrange -nGauss 4 -outPrec 6
 runNonlinel necking-2D-Q3-$h.inp -2D -UL -vtf 1 -nviz 4   -nGauss 4 -outPrec 6
 runNonlinel necking-2D-$h-Q3P2.inp -2D -MX 2 -vtf 1 -lagrange -nGauss 4 -outPrec 6 
 runNonlinel necking-2D-Q3P2-$h.inp -2D -MX 2 -vtf 1 -nviz 4   -nGauss 4 -outPrec 6
+runNonlinel Necking-2D-Q3P2-$h.inp -2D -MX 2 -vtf 1 -nviz 4   -nGauss 4 -outPrec 6
 runNonlinel necking-2D-$h-Q3Q2.inp -2D -mixed -vtf 1 -lagrange -nGauss 4 -outPrec 6
 runNonlinel necking-2D-Q3Q2-$h.inp -2D -mixed -vtf 1 -nviz 4   -nGauss 4 -outPrec 6
+runNonlinel Necking-2D-Q3Q2-$h.inp -2D -mixed -vtf 1 -nviz 4   -nGauss 4 -outPrec 6
 
 runNonlinel Necking-3D-Q2Q1-$h.inp -mixed -vtf 1 -nviz 3 -nGauss 3 -outPrec 6
 runNonlinel Necking-3D-Q3Q2-$h.inp -mixed -vtf 1 -nviz 4 -nGauss 4 -outPrec 6
