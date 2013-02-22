e2fd61a6a073e9ef74a05e276942372ebff3b95c
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2013-02-22 19:31:39 +0000

    Extended the run scripts to accept more command-line options
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2276 e10b68d5-8a6e-419e-a041-bce267b0401d

--- SquarePlate/runAMR.sh
+++ SquarePlate/runAMR.sh
@@ -4,13 +4,16 @@
 
 input=`basename $1 .inp`
 if [ ! -f $input.inp ]; then exec echo "usage: $0 <inputfile> [<ng>] [<nv>]"; fi
-if [ $# -gt 1 ]; then ng=$2; else ng=4; fi
-if [ $# -gt 2 ]; then nv=$3; else nv=2; fi
+if [ -z `which LinEl` ]; then exec echo "No program 'LinEl' in $PATH"; fi
+
+if [ $# -gt 1 ]; then shift; ng="-nGauss $1"; fi
+if [ $# -gt 1 ]; then shift; nv="-nviz $1"; fi
+shift
 
 output=${input}_AMR
-LinEl $input.inp -nGauss $ng -KL -adap -vtf 1 -nviz $nv | tee $output.log |\
+LinEl $input.inp $ng -KL -adap1 -vtf 1 $nv $* | tee $output.log |\
 egrep 'Adaptive|Number of elements|Exact relative'
 
-awk '/Number of unknowns/{ndof=$4};/Exact relative/{print ndof,$6}' $output.log > $output.dat
+awk '/Number of unknowns/{ndof=$4};/Exact relative/{exa=$6};/Relative error/{est=$5};/RMS/{print ndof,exa,est}' $output.log > $output.dat
 echo "\nDone. Resulting errors (stored in $output.dat):"
 cat $output.dat
--- SquarePlate/runUMR.sh
+++ SquarePlate/runUMR.sh
@@ -3,8 +3,8 @@
 # Uniform mesh refinement simulation for Kirchhoff-Love problems.
 
 input=`basename $1 .inp`
-nstep=$2
 if [ ! -f $input.inp -o $# -lt 2 ]; then exec echo "usage: $0 <inputfile> <nstep> [<ng>] [<nv>]"; fi
+if [ -z `which LinEl` ]; then exec echo "No program 'LinEl' in $PATH"; fi
 
 output=${input}_UMR
 if [ -f $output.log ]; then rm $output.log; fi
@@ -12,8 +12,10 @@ touch $output.log
 ru=`awk '/extra knots/{print$2}' $input.inp`
 rv=`awk '/extra knots/{print$3}' $input.inp`
 
-if [ $# -gt 2 ]; then ng=$3; else ng=4; fi
-if [ $# -gt 3 ]; then nv=$4; else nv=2; fi
+shift; nstep=$1
+if [ $# -gt 1 ]; then shift; ng="-nGauss $1"; fi
+if [ $# -gt 1 ]; then shift; nv="-nviz $1"; fi
+shift
 
 istep=0
 while [ $istep -lt $nstep ]; do
@@ -24,11 +26,11 @@ while [ $istep -lt $nstep ]; do
     sed "/extra knots/s/1  *[0-9][0-9]*  *[0-9][0-9]*/1 $ru $rv/" $input.inp > tmp.inp
   else cp $input.inp tmp.inp; fi
   echo "Running $input $((ru+1)) $((rv+1)) elements"
-  LinEl tmp.inp -nGauss $ng -KL -vtf 1 -nviz $nv >> $output.log
+  LinEl tmp.inp $ng -KL -vtf 1 $nv $* >> $output.log
   mv tmp.vtf ${input}_$((ru+1))_$((rv+1)).vtf
 done
 
 rm tmp.inp
-awk '/Number of unknowns/{ndof=$4};/Exact relative/{print ndof,$6}' $output.log > $output.dat
+awk '/Number of unknowns/{ndof=$4};/Exact relative/{exa=$6};/% of \|u\^r\|/{est=$8};/Profiling/{print ndof,exa,est}' $output.log > $output.dat
 echo "\nDone. Resulting errors (stored in $output.dat):"
 cat $output.dat
